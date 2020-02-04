09a91fdd661c216c096ac44c19e8c12c7fcb6e55
Jarell Reloj <jarellr@gmail.com>
2020-02-03 19:18:20 -0600

    Added simple tests

---
+++ src/main/java/com/thing/tests/SimpleTests.java
@@ -0,0 +1,29 @@
+package com.thing.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SimpleTests {
+
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+	}
+	
+	@BeforeMethod
+	public void before() {
+		System.out.println("Before Method");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("Test 3");
+	}
+	
+}
